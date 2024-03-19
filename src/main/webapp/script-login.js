a050ccadfab99502816592024bbe64f4d8e3775c
school00life <schl54lf@gmail.com>
2024-03-19 17:56:03 +0100

    my first commit

--- src/main/webapp/script-login.js
+++ src/main/webapp/script-login.js
@@ -1,3 +1,17 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+// قائمة المستخدمين
+// قائمة المستخدمين
+
 // استرجاع المستخدمين من التخزين المحلي إذا كانوا موجودين
 let users = JSON.parse(localStorage.getItem('users')) || [];
 
