019823552ae03235de16b9e9cea75a38b2950b98
school00life <schl54lf@gmail.com>
2024-03-19 17:52:46 +0100

    my first commit

--- src/main/webapp/script-login.js
+++ src/main/webapp/script-login.js
@@ -1,20 +1,5 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
-
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
-
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
-// قائمة المستخدمين
-// قائمة المستخدمين
-let users = [
-    { name: "zak", email: "zak@gmail.com", password: "123" },
-    
-];
+// استرجاع المستخدمين من التخزين المحلي إذا كانوا موجودين
+let users = JSON.parse(localStorage.getItem('users')) || [];
 
 document.querySelector('.sign-in form').addEventListener('submit', function(event) {
     event.preventDefault(); // منع الإرسال الافتراضي للنموذج
@@ -44,6 +29,9 @@ document.querySelector('.sign-up form').addEventListener('submit', function(even
     // إضافة مستخدم جديد إلى القائمة
     users.push({ name, email, password });
 
+    // حفظ المستخدمين في التخزين المحلي
+    localStorage.setItem('users', JSON.stringify(users));
+
     // إعادة توجيه المستخدم إلى صفحة تسجيل الدخول بعد التسجيل الناجح
-    window.location.href = 'home.html';
+    window.location.href = 'index.html';
 });
